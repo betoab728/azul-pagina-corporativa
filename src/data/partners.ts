aaf32b86be6f66085cf2ba1e4bf29cf633d72be5
Elias Alberto Alegre Barazorda <betoab728@gmail.com>
2026-06-11 13:39:18 -0500

    empresas

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -3,12 +3,32 @@ export interface Partner {
   logo: string
 }
 
-// TODO: Reemplazar con logos reales de clientes
-const placeholder = 'https://lh3.googleusercontent.com/aida-public/AB6AXuB3_x5R3n88746zC_YpS1gI8W0M4F3zI6kL8S2T9W4R7P5Q3M2L1K0O9I8H7G6F5E4D3C2B1A'
-
 export const partners: Partner[] = [
-  { name: 'Partner 1', logo: placeholder },
-  { name: 'Partner 2', logo: placeholder },
-  { name: 'Partner 3', logo: placeholder },
-  { name: 'Partner 4', logo: placeholder },
+  { name: 'Cliente 01', logo: '/cliente-01.png' },
+  { name: 'Cliente 02', logo: '/cliente-02.png' },
+  { name: 'Cliente 03', logo: '/cliente-03.png' },
+  { name: 'Cliente 04', logo: '/cliente-04.png' },
+  { name: 'Cliente 05', logo: '/cliente-05.png' },
+  { name: 'Cliente 06', logo: '/cliente-06.png' },
+  { name: 'Cliente 07', logo: '/cliente-07.png' },
+  { name: 'Cliente 08', logo: '/cliente-08.png' },
+  { name: 'Cliente 09', logo: '/cliente-09.png' },
+  { name: 'Cliente 10', logo: '/cliente-10.png' },
+  { name: 'Cliente 11', logo: '/cliente-11.png' },
+  { name: 'Cliente 12', logo: '/cliente-12.png' },
+  { name: 'Cliente 13', logo: '/cliente-13.png' },
+  { name: 'Cliente 14', logo: '/cliente-14.png' },
+  { name: 'Cliente 15', logo: '/cliente-15.png' },
+  { name: 'Cliente 16', logo: '/cliente-16.png' },
+  { name: 'Cliente 17', logo: '/cliente-17.png' },
+  { name: 'Cliente 18', logo: '/cliente-18.png' },
+  { name: 'Cliente 19', logo: '/cliente-19.png' },
+  { name: 'Cliente 20', logo: '/cliente-20.png' },
+  { name: 'Cliente 21', logo: '/cliente-21.png' },
+  { name: 'Cliente 22', logo: '/cliente-22.png' },
+  { name: 'Cliente 23', logo: '/cliente-23.png' },
+  { name: 'Cliente 24', logo: '/cliente-24.png' },
+  { name: 'Cliente 25', logo: '/cliente-25.png' },
+  { name: 'Cliente 26', logo: '/cliente-26.png' },
+  { name: 'Cliente 27', logo: '/cliente-27.png' },
 ]
